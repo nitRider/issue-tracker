44f710a16755586df6ae5e121b1423fc8e250801
Nitesh kumar <niteshkumar72@deloitte.com>
2023-07-21 18:02:26 +0530

    modified validation logic in create project screen

--- src/app/modules/create-project/create-project.component.ts
+++ src/app/modules/create-project/create-project.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 import { userRequest } from 'src/app/models/project.model';
 import { ApiService } from 'src/app/services/api.service';
@@ -10,23 +11,30 @@ import { ApiService } from 'src/app/services/api.service';
   styleUrls: ['./create-project.component.scss']
 })
 export class CreateProjectComponent implements OnInit {
+  isSubmitting = false;
+
   ownerList: userRequest[] = [];
 
   userData: any = [];
 
-  usernamePattern: string = '^[a-zA-Z0-9!/.|-]{5,150}$';
+  usernamePattern: string = '^[a-zA-Z0-9!/. |-]{5,150}$';
 
   projectForm = new FormGroup({
     projectName: new FormControl('', [
       Validators.required,
-      Validators.pattern(this.usernamePattern)
+      Validators.pattern(this.usernamePattern),
+      Validators.maxLength(150)
     ]),
     projectOwner: new FormControl('', [Validators.required]),
-    projectStartDate: new FormControl(''),
-    projectEndDate: new FormControl('')
+    projectStartDate: new FormControl('', [Validators.required]),
+    projectEndDate: new FormControl('', [Validators.required])
   });
 
-  constructor(private service: ApiService, private router: Router) {}
+  constructor(
+    private service: ApiService,
+    private router: Router,
+    private snackBar: MatSnackBar
+  ) {}
 
   ngOnInit(): void {
     this.service.getAllUser().subscribe((res) => {
@@ -37,13 +45,28 @@ export class CreateProjectComponent implements OnInit {
     });
   }
   onSubmit() {
-    var data = this.projectForm.value;
-    data.projectEndDate = data.projectEndDate.toISOString();
-    data.projectStartDate = data.projectStartDate.toISOString();
-    this.service.createProject(data).subscribe((res) => {
-      this.projectForm.reset();
-      this.router.navigate(['/']);
-    });
+    if (this.projectForm.valid && !this.isSubmitting) {
+      this.isSubmitting = true;
+      var data = this.projectForm.value;
+      data.projectEndDate = data.projectEndDate.toISOString();
+      data.projectStartDate = data.projectStartDate.toISOString();
+      // this.projectForm.reset();
+      this.service.createProject(data).subscribe(
+        (res: any) => {
+          this.snackBar.open('res.message', 'Ok', {
+            duration: 3000
+          });
+          this.router.navigate(['/']);
+        },
+        (err: any) => {
+          if (err?.status === 400) {
+            this.snackBar.open('projectID already exist', 'Undo', {
+              duration: 3000
+            });
+          }
+        }
+      );
+    }
   }
   reset() {
     this.projectForm.reset();
